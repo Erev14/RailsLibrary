ed0964670aefeaf03d9bc359e4c956ac80433512
Erev14 <erevjoach14@gmail.com>
2019-03-24 21:39:56 -0600

    define validation and relations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
+# Author's Moodel
 class Author < ApplicationRecord
+  has_many :books, inverse_of: :author
+  validates_associated :books
+
+  validates :name, presence: true, length: { in: 2..60 }
+  validates :father_last_name, presence: true, length: { in: 2..35 }
+  validates :mother_last_name, length: { in: 2..35 }, allow_nil: true,
+                               allow_blank: true
+  validates :nationality, length: { in: 2..35 }, allow_nil: true,
+                          allow_blank: true
+  validates :historical_review, length: { minimun: 2 }, allow_nil: true,
+                                allow_blank: true
 end
