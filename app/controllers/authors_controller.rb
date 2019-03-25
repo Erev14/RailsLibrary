961c11a019a77c3bfdcd39d5b83df751e528f29d
Erev14 <erevjoach14@gmail.com>
2019-03-25 10:01:15 -0600

    add CRUD

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,18 +3,41 @@
 # Author's Controller
 class AuthorsController < ApplicationController
   def list
-
+    @authors = Author.all
   end
 
- def show
+  def search
+    to_search = search_params
+    @author = Author.where(name: to_search[:name])
+                    .or(Author
+                      .where(father_last_name: to_search[:father_last_name]))
+                    .or(Author
+                      .where(mother_last_name: to_search[:mother_last_name]))
+  end
 
- end
+  def show
+    @author = Author.includes(:books).find(params[:id])
+  end
 
- def new
+  def new
+    @author = Author.new
+  end
 
- end
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to action: 'list'
+    else
+      render action: 'new'
+    end
+  end
 
- def create
-   
- end
+  def author_params
+    params.require(:author).permit(:name, :father_last_name, :mother_last_name,
+                                   :nationality, :historical_review)
+  end
+
+  def search_params
+    params.require(:author).permit(:name, :father_last_name, :mother_last_name)
+  end
 end
