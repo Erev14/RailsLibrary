90a87f4b4849a4e72fe5bc75fcb5ff747d14d8e6
Erev14 <erevjoach14@gmail.com>
2019-03-24 18:31:29 -0600

    define book case table

--- db/migrate/20190324222804_create_book_cases.rb
+++ db/migrate/20190324222804_create_book_cases.rb
@@ -1,7 +1,8 @@
 class CreateBookCases < ActiveRecord::Migration[5.2]
   def change
     create_table :book_cases do |t|
-
+      t.string :level, limit: 2, null: false
+      t.string :hall, limit: 100, null: false
       t.timestamps
     end
   end
