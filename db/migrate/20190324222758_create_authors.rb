8192dabb78aebcf5d60b01135c5239ddca7a1ff2
Erev14 <erevjoach14@gmail.com>
2019-03-24 18:31:07 -0600

    define authors table

--- db/migrate/20190324222758_create_authors.rb
+++ db/migrate/20190324222758_create_authors.rb
@@ -1,7 +1,11 @@
 class CreateAuthors < ActiveRecord::Migration[5.2]
   def change
     create_table :authors do |t|
-
+      t.string :name, limit: 60, null: false
+      t.string :father_last_name, limit: 35, null: false
+      t.string :mother_last_name, limit: 35
+      t.string :nationality, limit: 35
+      t.text :historical_review
       t.timestamps
     end
   end
