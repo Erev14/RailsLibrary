c93fc7413bf3e12ebff62f024341b63d1c995090
Erev14 <erevjoach14@gmail.com>
2019-03-25 10:02:22 -0600

    commented by now add validate false for not need relation completa

--- app/models/book_case.rb
+++ app/models/book_case.rb
@@ -2,8 +2,12 @@
 
 # Book Case's Moodel
 class BookCase < ApplicationRecord
-  belongs_to :book_case
+  belongs_to :book, required: false
 
-  validates :levels, presence: true, numericality: { only_integer: true }
-  validates :hall, presence: true, numericality: { only_integer: true }
+  # validates :levels, presence: true, numericality: { only_integer: true }
+  # validates :hall, presence: true, numericality: { only_integer: true }
+
+  def level_hall
+    "#{level}-#{hall}"
+  end
 end
