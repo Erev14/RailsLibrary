9b1da205f848b4a5c9c5e5710b53c66951e95fe7
Erev14 <erevjoach14@gmail.com>
2019-03-25 10:01:48 -0600

    commented by now

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,12 +5,15 @@ class Author < ApplicationRecord
   has_many :books, inverse_of: :author
   validates_associated :books
 
-  validates :name, presence: true, length: { in: 2..60 }
-  validates :father_last_name, presence: true, length: { in: 2..35 }
-  validates :mother_last_name, length: { in: 2..35 }, allow_nil: true,
-                               allow_blank: true
-  validates :nationality, length: { in: 2..35 }, allow_nil: true,
-                          allow_blank: true
-  validates :historical_review, length: { minimun: 2 }, allow_nil: true,
-                                allow_blank: true
+  # validates :name, presence: true, length: { in: 2..60 }
+  # validates :father_last_name, presence: true, length: { in: 2..35 }
+  # validates :mother_last_name, length: { in: 2..35 }, allow_nil: true,
+  #                              allow_blank: true
+  # validates :nationality, length: { in: 2..35 }, allow_nil: true,
+  #                         allow_blank: true
+  # validates :historical_review, length: { minimun: 2 }, allow_nil: true,
+  #                               allow_blank: true
+  def name_with_initial
+    "#{name} #{father_last_name} #{mother_last_name}"
+  end
 end
