88aa71eddcc7ebd2fc3fc5334666b248d35a079f
Erev14 <erevjoach14@gmail.com>
2019-03-25 10:07:30 -0600

    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,16 +3,17 @@
 # Book's Moodel
 class Book < ApplicationRecord
   belongs_to :author
-  has_one :book_case
-  validates :title, presence: true, length: { in: 2..250 }
-  validates :editorial, presence: true, length: { in: 2..250 }
-  validates :edition_year, length: { is: 4 }, allow_nil: true,
-                           numericality: { only_integer: true,
-                                           allow_nil: true },
-                           allow_blank: true
-  validates :edition_number
-  validates :ISBN, presence: true, length: { in: 10..13 }, uniqueness: true,
-                   numericality: { only_integer: true }
+  belongs_to :book_case
+  # validates :title, presence: true, length: { in: 2..250 }, on: :create
+  # validates :editorial, presence: true, length: { in: 2..250 }, on: :create
+  # validates :edition_year, length: { is: 4 }, allow_nil: true,
+  #                          numericality: { only_integer: true,
+  #                                          allow_nil: true },
+  #                          allow_blank: true, on: :create
+  # validates :edition_number, on: :create
+  # validates :ISBN, presence: true, length: { in: 10..13 }, uniqueness: true,
+  #                  numericality: { only_integer: true }, on: :create
+
   # validates :author, presence: true
   # validates :book_case, presence: true
 end
