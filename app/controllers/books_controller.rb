e39c7cb780694468819be43aa762cde83c4494fd
Erev14 <erevjoach14@gmail.com>
2019-03-24 21:44:26 -0600

    add functions to list, show, new and create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,25 @@
 
 # Book's Controller
 class BooksController < ApplicationController
-  
+  def list
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to action: 'list'
+    else
+      @subjects = Subject.all
+      render action: 'new'
+    end
+  end
 end
