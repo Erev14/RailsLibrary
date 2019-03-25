7971e41958d98edb75837ff81c1efd18cb1701ae
Erev14 <erevjoach14@gmail.com>
2019-03-24 21:38:50 -0600

    add needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
+# declarete routes for Get and POST
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :author, only: %i[index new create show] do
+    get 'search', on: :collection
+    resource :book, only: [:show]
+  end
+
+  resource :book, only: %i[index new create show] do
+    get 'search', on: :collection
+  end
+
+  resource :bookcase, only: %i[index new create show]
 end
