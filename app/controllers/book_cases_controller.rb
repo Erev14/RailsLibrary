598f1ec2981e11e3d0d343d553dfe839f642a4e6
Erev14 <erevjoach14@gmail.com>
2019-03-24 21:42:17 -0600

    add files generated by rails generate controller BookCases

---
+++ app/controllers/book_cases_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Book's Controller
+class BookCasesController < ApplicationController
+  def list
+    
+  end
+
+  def show
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+end
