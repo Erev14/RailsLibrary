884f9d176d0af7a90ca73350281330604dea52f2
Erev14 <erevjoach14@gmail.com>
2019-03-24 21:39:45 -0600

    define validations and relations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
+# Book's Moodel
 class Book < ApplicationRecord
+  belongs_to :author
+  has_one :book_case
+  validates :title, presence: true, length: { in: 2..250 }
+  validates :editorial, presence: true, length: { in: 2..250 }
+  validates :edition_year, length: { is: 4 }, allow_nil: true,
+                           numericality: { only_integer: true,
+                                           allow_nil: true },
+                           allow_blank: true
+  validates :edition_number
+  validates :ISBN, presence: true, length: { in: 10..13 }, uniqueness: true,
+                   numericality: { only_integer: true }
+  # validates :author, presence: true
+  # validates :book_case, presence: true
 end
