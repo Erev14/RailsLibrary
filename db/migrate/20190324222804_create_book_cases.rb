28c4016886492532a398aed74b9e1ae22d0937ee
Erev14 <erevjoach14@gmail.com>
2019-03-25 10:06:22 -0600

    change limits

--- db/migrate/20190324222804_create_book_cases.rb
+++ db/migrate/20190324222804_create_book_cases.rb
@@ -1,7 +1,7 @@
 class CreateBookCases < ActiveRecord::Migration[5.2]
   def change
     create_table :book_cases do |t|
-      t.string :level, limit: 2, null: false
+      t.string :level, limit: 5, null: false
       t.string :hall, limit: 100, null: false
       t.timestamps
     end
