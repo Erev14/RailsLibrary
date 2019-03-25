b4f9f560b5edd530545c3aa71df8272809bbb62f
Erev14 <erevjoach14@gmail.com>
2019-03-24 21:42:00 -0600

    add files generated by rails generate controller Authors

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Author's Controller
+class AuthorsController < ApplicationController
+  def list
+
+  end
+
+ def show
+
+ end
+
+ def new
+
+ end
+
+ def create
+   
+ end
+end
