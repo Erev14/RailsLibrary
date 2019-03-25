ab9b74f0775515dc0ef6a342dd187f5563deaa5e
Erev14 <erevjoach14@gmail.com>
2019-03-24 21:39:32 -0600

    define relations and validations

--- app/models/book_case.rb
+++ app/models/book_case.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
+# Book Case's Moodel
 class BookCase < ApplicationRecord
+  belongs_to :book_case
+
+  validates :levels, presence: true, numericality: { only_integer: true }
+  validates :hall, presence: true, numericality: { only_integer: true }
 end
